a7a84ff5c51b9136aebf3d4dbf06747dcbac534a
João Pedro Gomes Ruas <gerenciaminas5@gmail.com>
2024-06-20 16:31:34 -0300

    2 atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,8 @@
-async function buscarRepos() {
+function irParaRepos() {
     const username = document.getElementById('username').value;
-
     if (!username) {
         alert('Por favor, digite um nome de usuário.');
         return;
     }
-
-    try {
-        const response = await fetch(`https://api.github.com/users/${username}/repos`);
-        if (!response.ok) {
-            throw new Error('Usuário não encontrado');
-        }
-        const repos = await response.json();
-        localStorage.setItem('repositoriosUsuarioGitHub', JSON.stringify(repos));
-        window.location.href = 'repositorio.html';
-    } catch (error) {
-        alert(error.message);
-    }
+    window.location.href = `repositorio.html?username=${username}`;
 }
--- assets/js/repositorio.js
+++ assets/js/repositorio.js
@@ -1,16 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    const params = new URLSearchParams(window.location.search);
+    const username = params.get('username');
     const repoList = document.getElementById('repo-list');
-    const repos = JSON.parse(localStorage.getItem('repositoriosUsuarioGitHub'));
 
-    if (!repos || repos.length === 0) {
-        repoList.innerHTML = '<li>Nenhum repositório encontrado.</li>';
+    if (!username) {
+        repoList.innerHTML = '<li>Nome de usuário não fornecido.</li>';
         return;
     }
 
-    repoList.innerHTML = repos.map(repo => `
-        <li>
-            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-            <p>${repo.description ? repo.description : 'Sem descrição'}</p>
-        </li>
-    `).join('');
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}/repos`);
+        if (!response.ok) {
+            throw new Error('Usuário não encontrado');
+        }
+        const repos = await response.json();
+        if (repos.length === 0) {
+            repoList.innerHTML = '<li>Nenhum repositório encontrado.</li>';
+            return;
+        }
+
+        repoList.innerHTML = repos.map(repo => {
+            const creationDate = new Date(repo.created_at).toLocaleDateString('pt-BR');
+            return `
+                <li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                    <p>${repo.description ? repo.description : 'Sem descrição'}</p>
+                    <p>Criado em: ${creationDate}</p>
+                </li>
+            `;
+        }).join('');
+    } catch (error) {
+        repoList.innerHTML = `<li>${error.message}</li>`;
+    }
 });
